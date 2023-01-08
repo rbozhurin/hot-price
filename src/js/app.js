84592004e2f3de25a4d987af2d1cc3f3bf8b8442
Rayko Bozhurin <rbozhurin@gmail.com>
2023-01-08 19:44:49 +0200

    fixes

--- src/js/app.js
+++ src/js/app.js
@@ -3,3 +3,12 @@ import "../scss/app.scss";
 window.addEventListener("DOMContentLoaded", () => {
   // This block will be executed once the page is loaded and ready
 });
+
+const prices = document.querySelectorAll(".price");
+
+prices.forEach((price) => {
+  if (price.classList.contains("hot")) {
+    const fireEmoji = document.createTextNode("🔥");
+    price.appendChild(fireEmoji);
+  }
+});
